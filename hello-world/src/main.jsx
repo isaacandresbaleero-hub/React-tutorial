8829401479a863ff815bf9850b0977d944302d08
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-19 21:12:56 +0300

    more update on main.jsx

--- hello-world/src/main.jsx
+++ hello-world/src/main.jsx
@@ -1,12 +1,6 @@
-import { StrictMode } from 'react'
-import { createRoot } from "react-dom/client"
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from 'react-dom/client'
 
-createRoot ( document.getElementById('root')) .render(
+createRoot ( document.getElementById('root')). render(
 
-    <StrictMode>
-      <App/>
-
-    </StrictMode>
+  <h1> Hello React!</h1>
 )
