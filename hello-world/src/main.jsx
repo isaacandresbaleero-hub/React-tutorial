67715ebd226faf51d424c78c8bfe6ecf85e86244
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-19 21:02:01 +0300

    corrected more errors in main.jsx

--- hello-world/src/main.jsx
+++ hello-world/src/main.jsx
@@ -1,14 +1,12 @@
-import { StrictMode } from "react";
-import { createRoom, createRoot } from "react-dom/client"
-
-import App from './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from "react-dom/client"
+import './index.css'
 import App from './App.jsx'
 
 createRoot ( document.getElementById('root')) .render(
 
     <StrictMode>
-      <App>
-        
-      </App>
+      <App/>
+
     </StrictMode>
 )
