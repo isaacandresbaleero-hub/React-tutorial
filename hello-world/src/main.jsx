9ddb8edb0e77fb4c503d1a929141129fc852b74c
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-20 23:44:29 +0300

    updates main.js where app starts

--- hello-world/src/main.jsx
+++ hello-world/src/main.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { StrictMode } from 'react'
 import ReactDom from "@vitejs/plugin-react"
 import { BrowserRouter } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 import APP from './App'
 import { AuthProvider } from './contexts/Authcontext'
 import 'index.css'
+
+ReactDom.createRoot(
+  document.getElementById('root')).render(
+<React.StrictMode>
+ <BrowserRouter>
+  <AuthProvider>
+     <App/>
+      <Toaster position = "Top Right"/>
+   </AuthProvider>
+  </BrowserRouter>
+</React.StrictMode>
+
+
+
+
+
+  )
+
