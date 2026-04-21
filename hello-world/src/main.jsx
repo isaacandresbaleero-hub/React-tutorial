8a6d57ac3becf50212bca7b8642fb0c86f76bef5
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-22 00:11:46 +0300

    correcting errors

--- hello-world/src/main.jsx
+++ hello-world/src/main.jsx
@@ -1,25 +1,18 @@
-import React, { StrictMode } from 'react'
-import ReactDom from "@vitejs/plugin-react"
-import { BrowserRouter } from 'react-router-dom'
-import { Toaster } from 'react-hot-toast'
-import APP from './App'
-import { AuthProvider } from './contexts/Authcontext'
-import 'index.css'
+import React, { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
+import './index.css';
 
-ReactDom.createRoot(
-  document.getElementById('root')).render(
-<React.StrictMode>
- <BrowserRouter>
-  <AuthProvider>
-     <App/>
-      <Toaster position = "Top Right"/>
-   </AuthProvider>
-  </BrowserRouter>
-</React.StrictMode>
-
-
-
-
-
-  )
-
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <AuthProvider>
+        <App />
+        <Toaster position="top-right" />
+      </AuthProvider>
+    </BrowserRouter>
+  </StrictMode>
+);
