24989971f65006a74dd80bec0d2889f30dd29392
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-21 23:37:35 +0300

    more update on AuthContext

--- hello-world/src/context/AuthContext.jsx
+++ hello-world/src/context/AuthContext.jsx
@@ -1,6 +1,25 @@
-import React, { createContext, useState, useContext, useEffect } from 'react';
+import React { createContext, useState, useContext, useEffect } from 'react';
 import { login as apiLogin, register as apiRegister } from '../services/api';
 import toast from 'react-hot-toast';
 
 const AuthContext = createContext();
 
+export const useAuth = () => {
+    const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+};
+
+export const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [token, setToken] = useState(localStorage.getItem('access_token'));
+
+    useEffect(() => {
+         const loadUser = async () => {
+            const storedUser = localStorage.getItem('user');
+            const storedToken = localStorage.getItem('access_token');
+
+            
