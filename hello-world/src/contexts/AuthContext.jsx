ef4a8e577ed2a881f4288702e0f6ce8d2d70e364
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-22 22:24:31 +0300

    mede changes in Auth context

--- hello-world/src/contexts/AuthContext.jsx
+++ hello-world/src/contexts/AuthContext.jsx
@@ -1,4 +1,4 @@
-import React { createContext, useState, useContext, useEffect } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react'; 
 import { login as apiLogin, register as apiRegister } from '../services/api';
 import toast from 'react-hot-toast';
 
