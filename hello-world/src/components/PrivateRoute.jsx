e24d52f3b0b02c9956562b74e13c0e228f1d537d
isaacandresbaleero-hub <isaacandresbaleero@gmail.com>
2026-04-22 22:35:21 +0300

    updated private route.jsx

---
+++ hello-world/src/components/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+const PrivateRoute = () => {
+  const { user, loading } = useAuth();
+
+  if (loading) {
+    return <div>Loading...</div>; // Or a nice spinner
+  }
+
+  // If there is no user, redirect them to the login page
+  return user ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
